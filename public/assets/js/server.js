924b1a1fcce2f52b2afc4b73562f0238fbdecbe6
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-05-06 16:21:07 -0400

    initial function, testing

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,3 +2,12 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+
+// GET REQUEST
+
+app.get('/api/notes', function(req, res) {
+    fs.readFile('db/db.json', 'utf8', function(error, content) {
+        let note = JSON.parse(content);
+        res.send(note);
+    });
+});
