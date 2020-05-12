97eb2963eacfc3f524dd7600a320227230ec1ae6
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-05-12 08:28:55 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -11,15 +11,12 @@ var db = require('/db/db.json');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-
-// GET REQUEST
+// GET request
 app.get('/', function(req, res) {
     res.sendFile(path.join(_dirname, 'index.html'));
 });
 app.get('/notes', function(req, res) {
-    res.sendFile(path.join(_dirname, 'notes.html'));
+    res.sendFile(path.join(_dirname, '/notes.html'));
 });
 app.get('/api/notes', function(req, res) {
     return res.json(db);
@@ -27,39 +24,29 @@ app.get('/api/notes', function(req, res) {
 
 // POST request
 app.post('/api/notes', function(req, res) {
-    
+    var newNote = req.body;
+    console.log(newNote);
+    waiting.push(newNote);
+    res.json(newNote);
 })
 
 // DELETE request
-
-app.delete('/api/notes/:id', function(req, res) {
-    fs.readFile('db/db.json', function(error, data) {
-        if (error) throw error;
-        let deleteID = req.params.id;
-        let json = JSON.parse(data);
-        json.forEach(function(item, i) {
-            if (item.id.includes(deleteID)) {
-                json.splice(i, 1);
-            }
-        })
-        fs.writeFile('db/db.json', JSON.stringify(json,null,2), function(error) {
-            if (error) throw error;
-            res.send('200');
-        })
-    })
-})
-
-// routes
-app.get('/', function(req, res) {
-    res.sendFile(path.join(_dirname, 'public', 'index.html'));
-})
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(_dirname, 'public', 'notes.html'));
-})
-
-// get css/js files
-app.use(express.static(path.join(__dirname, 'public')));
+// app.delete('/api/notes/:id', function(req, res) {
+//     fs.readFile('db/db.json', function(error, data) {
+//         if (error) throw error;
+//         let deleteID = req.params.id;
+//         let json = JSON.parse(data);
+//         json.forEach(function(item, i) {
+//             if (item.id.includes(deleteID)) {
+//                 json.splice(i, 1);
+//             }
+//         })
+//         fs.writeFile('db/db.json', JSON.stringify(json,null,2), function(error) {
+//             if (error) throw error;
+//             res.send('200');
+//         })
+//     })
+// })
 
 app.listen(PORT, function() {
     console.log("Listening on port: " + PORT);
